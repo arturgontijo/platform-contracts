99232c0b3a63d9a590cfb764e453db693faa9ee0
arturgontijo <arturgontijo@gmail.com>
2019-03-21 11:23:18 -0300

    [MPE] Add openChannelByThirdParty.

--- contracts/MultiPartyEscrow.sol
+++ contracts/MultiPartyEscrow.sol
@@ -28,13 +28,16 @@ contract MultiPartyEscrow {
     mapping (uint256 => PaymentChannel) public channels;
     mapping (address => uint256)        public balances; //tokens which have been deposit but haven't been escrowed in the channels
    
+    mapping (bytes32 => bool) public usedMessages;
+
     uint256 public nextChannelId; //id of the next channel (and size of channels)
- 
+
     ERC20 public token; // Address of token contract
-    
+
 
     // Events
     event ChannelOpen(uint256 channelId, uint256 nonce, address indexed sender, address signer, address indexed recipient, bytes32 indexed groupId, uint256 amount, uint256 expiration);
+    event ChannelOpenByThirdParty(uint256 channelId, uint256 nonce, address thirdParty, address indexed signer, address indexed recipient, bytes32 indexed groupId, uint256 amount, uint256 expiration, uint256 fees, uint256 messageNonce);
     event ChannelClaim(uint256 indexed channelId, uint256 nonce, address indexed recipient, uint256 claimAmount, uint256 sendBackAmount, uint256 keepAmount);
     event ChannelSenderClaim(uint256 indexed channelId, uint256 nonce, uint256 claimAmount);
     event ChannelExtend(uint256 indexed channelId, uint256 newExpiration);
@@ -48,17 +51,27 @@ contract MultiPartyEscrow {
     {
         token = ERC20(_token);
     }
-  
-    function deposit(uint256 value) 
+
+    function deposit(uint256 value)
     public
-    returns(bool) 
+    returns(bool)
     {
-        require(token.transferFrom(msg.sender, this, value), "Unable to transfer token to the contract"); 
+        require(token.transferFrom(msg.sender, this, value), "Unable to transfer token to the contract");
         balances[msg.sender] = balances[msg.sender].add(value);
         emit DepositFunds(msg.sender, value);
         return true;
     }
-    
+
+    function depositR(address sender, uint256 value)
+    public
+    returns(bool)
+    {
+        require(token.transferFrom(sender, this, value), "Unable to transfer token to the contract");
+        balances[sender] = balances[sender].add(value);
+        emit DepositFunds(sender, value);
+        return true;
+    }
+
     function withdraw(uint256 value)
     public
     returns(bool)
@@ -69,7 +82,7 @@ contract MultiPartyEscrow {
         emit WithdrawFunds(msg.sender, value);
         return true;
     }
-    
+
     function transfer(address receiver, uint256 value)
     public
     returns(bool)
@@ -81,8 +94,8 @@ contract MultiPartyEscrow {
         emit TransferFunds(msg.sender, receiver, value);
         return true;
     }
-    
-    
+
+
     //open a channel, token should be already being deposit
     //openChannel should be run only once for given sender, recipient, groupId
     //channel can be reused even after channelClaim(..., isSendback=true)
@@ -102,14 +115,68 @@ contract MultiPartyEscrow {
             value        : value,
             expiration   : expiration
         });
-      
-        balances[msg.sender] = balances[msg.sender].sub(value);  
+
+        balances[msg.sender] = balances[msg.sender].sub(value);
         emit ChannelOpen(nextChannelId, 0, msg.sender, signer, recipient, groupId, value, expiration);
         nextChannelId += 1;
         return true;
     }
-    
 
+    function openChannelByThirdParty(address signer, address recipient, bytes32 groupId, uint256 value, uint256 expiration, uint256 fees, uint256 messageNonce, bytes memory signature)
+    public
+    returns(bool)
+    {
+        require(balances[signer] >= value + fees, "Insufficient funds at Signer MPE wallet.");
+        require(signer != address(0), "Signet is the Contract's Owner.");
+
+        // compose the message which was signed
+        bytes32 message = prefixed(keccak256(abi.encodePacked(this, msg.sender, signer, recipient, groupId, value, expiration, fees, messageNonce)));
+
+        require(checkSignature(signer, message, signature), "Invalid Signature.");
+
+        // check for replay attack (message can be used only once)
+        require(!usedMessages[message], "Message already used.");
+        usedMessages[message] = true;
+
+        channels[nextChannelId] = PaymentChannel({
+            nonce        : 0,
+            sender       : signer,
+            signer       : signer,
+            recipient    : recipient,
+            groupId      : groupId,
+            value        : value,
+            expiration   : expiration
+        });
+
+        // Transferring Fees to ThirdParty
+        balances[signer] = balances[signer].sub(fees);
+        balances[msg.sender] = balances[msg.sender].add(fees);
+
+        // Transferring to Channel
+        balances[signer] = balances[signer].sub(value);
+
+        emit ChannelOpenByThirdParty(nextChannelId, 0, msg.sender, signer, recipient, groupId, value, expiration, fees, messageNonce);
+        nextChannelId += 1;
+        return true;
+    }
+
+    function checkSignature(address signer, bytes32 message, bytes memory signature)
+    public
+    pure
+    returns (bool)
+    {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+        if (v < 27) v += 27;
+
+        return ecrecover(message, v, r, s) == signer;
+    }
 
     function depositAndOpenChannel(address signer, address recipient, bytes32 groupId, uint256 value, uint256 expiration)
     public
@@ -126,7 +193,7 @@ contract MultiPartyEscrow {
     {
         PaymentChannel storage channel = channels[channelId];
 
-        balances[channel.sender] = balances[channel.sender].add(channel.value); 
+        balances[channel.sender] = balances[channel.sender].add(channel.value);
         channel.value            = 0;
         channel.nonce           += 1;
         channel.expiration       = 0;
@@ -141,43 +208,43 @@ contract MultiPartyEscrow {
      * @param r channel senders signatures in V R S for each channel
      * @param s channel senders signatures in V R S for each channel
      */
-    function multiChannelClaim(uint256[] channelIds, uint256[] amounts, bool[] isSendbacks, uint8[] v, bytes32[] r, bytes32[] s) 
-    public 
+    function multiChannelClaim(uint256[] channelIds, uint256[] amounts, bool[] isSendbacks, uint8[] v, bytes32[] r, bytes32[] s)
+    public
     {
         uint256 len = channelIds.length;
-        
+
         require(amounts.length == len && isSendbacks.length == len && v.length == len && r.length == len && s.length == len);
         for(uint256 i=0; i<len ; i++) {
             channelClaim(channelIds[i], amounts[i], v[i], r[i], s[i], isSendbacks[i]);
         }
-        
+
     }
 
-    function channelClaim(uint256 channelId, uint256 amount, uint8 v, bytes32 r, bytes32 s, bool isSendback) 
-    public 
+    function channelClaim(uint256 channelId, uint256 amount, uint8 v, bytes32 r, bytes32 s, bool isSendback)
+    public
     {
         PaymentChannel storage channel = channels[channelId];
         require(amount <= channel.value);
         require(msg.sender == channel.recipient);
-        
+
         //compose the message which was signed
         bytes32 message = prefixed(keccak256(abi.encodePacked(this, channelId, channel.nonce, amount)));
         // check that the signature is from the signer
         address signAddress = ecrecover(message, v, r, s);
         require(signAddress == channel.signer);
-        
+
         //transfer amount from the channel to the sender
         channel.value        =        channel.value.sub(amount);
         balances[msg.sender] = balances[msg.sender].add(amount);
-   
-        if (isSendback)    
+
+        if (isSendback)
             {
                 _channelSendbackAndReopenSuspended(channelId);
                 emit ChannelClaim(channelId, channel.nonce, msg.sender, amount, channel.value, 0);
             }
             else
             {
-                //reopen new "channel", without sending back funds to "sender"        
+                //reopen new "channel", without sending back funds to "sender"
                 channel.nonce += 1;
                 emit ChannelClaim(channelId, channel.nonce, msg.sender, amount, 0, channel.value);
             }
@@ -186,8 +253,8 @@ contract MultiPartyEscrow {
 
 
     /// the sender can extend the expiration at any time
-    function channelExtend(uint256 channelId, uint256 newExpiration) 
-    public 
+    function channelExtend(uint256 channelId, uint256 newExpiration)
+    public
     returns(bool)
     {
         PaymentChannel storage channel = channels[channelId];
@@ -196,11 +263,11 @@ contract MultiPartyEscrow {
         require(newExpiration >= channel.expiration);
 
         channels[channelId].expiration = newExpiration;
-        
+
         emit ChannelExtend(channelId, newExpiration);
         return true;
     }
-    
+
     /// the sender could add funds to the channel at any time
     /// any one can fund the channel irrespective of the sender
     function channelAddFunds(uint256 channelId, uint256 amount)
@@ -212,7 +279,7 @@ contract MultiPartyEscrow {
         //tranfser amount from sender to the channel
         balances[msg.sender]      = balances[msg.sender].sub     (amount);
         channels[channelId].value = channels[channelId].value.add(amount);
-        
+
         emit ChannelAddFunds(channelId, amount);
         return true;
     }
@@ -223,24 +290,21 @@ contract MultiPartyEscrow {
         require(channelExtend(channelId, newExpiration));
         require(channelAddFunds(channelId, amount));
     }
-    
-    // sender can claim refund if the timeout is reached 
-    function channelClaimTimeout(uint256 channelId) 
-    public 
+
+    // sender can claim refund if the timeout is reached
+    function channelClaimTimeout(uint256 channelId)
+    public
     {
         require(msg.sender == channels[channelId].sender);
         require(block.number >= channels[channelId].expiration);
         _channelSendbackAndReopenSuspended(channelId);
-        
+
         emit ChannelSenderClaim(channelId, channels[channelId].nonce, channels[channelId].value);
     }
 
-
     /// builds a prefixed hash to mimic the behavior of ethSign.
-    function prefixed(bytes32 hash) internal pure returns (bytes32) 
+    function prefixed(bytes32 hash) internal pure returns (bytes32)
     {
         return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
     }
-    
-    
 }
